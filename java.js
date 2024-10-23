ee8a73df34819482249a8fcbe536fbb53575b81c
Egan-ombaka <eganombaka97@gmail.com>
2024-10-23 12:05:59 +0300

    eganh.dev

--- java.js
+++ java.js
@@ -2,3 +2,31 @@ document.getElementById("aboutButton").addEventListener("click",
 function() {
     this.classList.add("active");
 });
+
+
+const download_button =
+            document.getElementById('downloadCv');
+        const content =
+            document.getElementById('content');
+
+        download_button.addEventListener
+            ('click', async function () {
+                const filename = 'table_data.pdf';
+
+                try {
+                    const opt = {
+                        margin: 1,
+                        filename: filename,
+                        image: { type: 'jpeg', quality: 0.98 },
+                        html2canvas: { scale: 2 },
+                        jsPDF: {
+                            unit: 'in', format: 'letter',
+                            orientation: 'portrait'
+                        }
+                    };
+                    await html2pdf().set(opt).
+                        from(content).save();
+                } catch (error) {
+                    console.error('Error:', error.message);
+                }
+            });
